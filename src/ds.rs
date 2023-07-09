faf351bdd7ab54d98c43d6d7ad942ed2e4084cf8
becknik <jannikb@posteo.de>
2023-07-09 14:43:42 +0200

    Rollback to provided templates state (which works)...

--- src/ds.rs
+++ src/ds.rs
@@ -1,12 +1,14 @@
-use mill_playfield::{FieldPos, FieldState, EfficientPlayField};
+use std::str::FromStr;
 
 pub const SIDES: usize = 4;
 pub const SIDESXTWO: usize = SIDES * 2;
 
-pub const POSITIONS: [FieldPos; 3 * SIDESXTWO] = [
-    FieldPos{ ring_index: 2, index: 0}, FieldPos{ ring_index: 2, index: 1}, FieldPos{ ring_index: 2, index: 2}, FieldPos{ ring_index: 2, index: 3}, FieldPos{ ring_index: 2, index: 4}, FieldPos{ ring_index: 2, index: 5}, FieldPos{ ring_index: 2, index: 6}, FieldPos{ ring_index: 2, index: 7},
-    FieldPos{ ring_index: 1, index: 0}, FieldPos{ ring_index: 1, index: 1}, FieldPos{ ring_index: 1, index: 2}, FieldPos{ ring_index: 1, index: 3}, FieldPos{ ring_index: 1, index: 4}, FieldPos{ ring_index: 1, index: 5}, FieldPos{ ring_index: 1, index: 6}, FieldPos{ ring_index: 1, index: 7},
-    FieldPos{ ring_index: 0, index: 0}, FieldPos{ ring_index: 0, index: 1}, FieldPos{ ring_index: 0, index: 2}, FieldPos{ ring_index: 0, index: 3}, FieldPos{ ring_index: 0, index: 4}, FieldPos{ ring_index: 0, index: 5}, FieldPos{ ring_index: 0, index: 6}, FieldPos{ ring_index: 0, index: 7},
+pub type Pos = (u8, u8);
+
+pub const POSITIONS: [Pos; 3 * SIDESXTWO] = [
+    (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7),
+    (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7),
+    (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7),
 ];
 
 #[derive(Copy, Clone, PartialEq)]
@@ -17,45 +19,112 @@ pub enum Phase {
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum Action {
-    Place(FieldPos),
-    Move(FieldPos, FieldPos),
+    Place(Pos),
+    Move(Pos, Pos),
 }
 
-#[derive(Copy, Clone, PartialEq)]
-pub struct Move {
-    pub(crate) action: Action,
-    pub(crate) take: Option<FieldPos>,
-}
-
-impl ToString for Move {
-    fn to_string(&self) -> String {
-        format!("{}{}",
-            match self.action {
-                Action::Place(t) => { format!("P {}", pos_to_string(t)) }
-                Action::Move(s, t) => { format!("M {} {}", pos_to_string(s), pos_to_string(t))}
-            },
-            &match self.take {
-                None => { String::new() }
-                Some(t) => { format!(" T {}", pos_to_string(t)) }
-            }
-        )
-    }
-}
-
-pub fn apply_action(conf: &EfficientPlayField, action: Action, color: FieldState) -> EfficientPlayField {
+pub fn apply_action(conf: &Configuration, action: Action, color: State) -> Configuration {
     let mut conf = conf.clone();
     match action {
         Action::Place(p) => {
-            conf.set_field_state(p, color);
+            conf.set(p, color);
         }
         Action::Move(from, to) => {
-            conf.set_field_state(to, conf.get_field_state(from));
-            conf.set_field_state(from, FieldState::Free);
+            conf.set(to, conf.get(from));
+            conf.set(from, State::Empty);
         }
     }
     conf
 }
 
-fn pos_to_string(p: FieldPos) -> String {
+#[derive(Copy, Clone, PartialEq)]
+pub struct Move {
+    pub(crate) action: Action,
+    pub(crate) take: Option<Pos>,
+}
+
+fn pos_to_string(p: Pos) -> String {
     format!("{}", POSITIONS.iter().position(|&p1| p == p1).unwrap() + 1)
+}
+
+impl ToString for Move {
+    fn to_string(&self) -> String {
+        format!("{}{}",
+        match self.action {
+            Action::Place(t) => { format!("P {}", pos_to_string(t)) }
+            Action::Move(s, t) => { format!("M {} {}", pos_to_string(s), pos_to_string(t))}
+        },
+        match self.take {
+            None => { String::new() }
+            Some(t) => { format!(" T {}", pos_to_string(t)) }
+        }
+        )
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Copy, Clone, Default, Debug)]
+pub enum State {
+    #[default]
+    Empty,
+    White,
+    Black,
+}
+
+impl State {
+    pub fn flip_color(&self) -> State {
+        match self {
+            State::Empty => {
+                State::Empty
+            }
+            State::White => {
+                State::Black
+            }
+            State::Black => {
+                State::White
+            }
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Copy, Clone, Default, Debug)]
+pub struct Configuration {
+    arr: [[State; SIDESXTWO]; 3],
+}
+
+impl Configuration {
+    pub(crate) fn get(&self, p: Pos) -> State {
+        let (r, i) = p;
+        self.arr[r as usize][i as usize]
+    }
+
+    pub(crate) fn set(&mut self, p: Pos, state: State) {
+        let (r, i) = p;
+        self.arr[r as usize][i as usize] = state;
+    }
+}
+
+impl FromStr for Configuration {
+    type Err = ();
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let mut conf = Configuration::default();
+        let mut iter = line.chars();
+        for p in POSITIONS {
+            match iter.next().unwrap() {
+                'W' => {
+                    conf.set(p, State::White);
+                }
+                'B' => {
+                    conf.set(p, State::Black);
+                }
+                'E' => {
+                    conf.set(p, State::Empty);
+                }
+                _ => {
+                    return Err(());
+                }
+            }
+        }
+        Ok(conf)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,77 @@
-use mill_playfield::{EfficientPlayField, FieldPos, FieldState};
 use nanorand::{WyRand, Rng};
 use once_cell::sync::Lazy;
 
-use crate::ds::{Action, apply_action, Move, Phase, POSITIONS};
+use crate::ds::{Action, apply_action, Configuration, Move, Phase, Pos, POSITIONS, State};
+
+static mut RNG: Lazy<WyRand> = Lazy::new(|| WyRand::new());
 
 mod ds;
-mod mcts;
 
-static mut RNG: Lazy<WyRand> = Lazy::new(|| WyRand::default());
-
-fn is_corner(p: FieldPos) -> bool {
-    (p.index % 2) == 1
+fn is_corner(p: Pos) -> bool {
+    let (_, i) = p;
+    (i % 2) == 1
 }
 
-pub fn is_muhle(conf: &EfficientPlayField, p: FieldPos) -> bool {
-    let FieldPos{ring_index, index: i} = p;
-    let state = conf.get_field_state(p);
-
+pub fn is_muhle(conf: &Configuration, p: Pos) -> bool {
+    let (r, i) = p;
+    let state = conf.get(p);
     if is_corner(p) {
-        (conf.get_field_state(FieldPos{ring_index, index: (i + 1) % 8}) == state
-            && conf.get_field_state(FieldPos{ring_index, index: (i + 2) % 8}) == state)
-        || (conf.get_field_state(FieldPos{ring_index, index: (i + 8 - 2) % 8}) == state
-            && conf.get_field_state(FieldPos { ring_index, index: (i + 8 - 1) % 8}) == state)
+        (conf.get((r, (i + 1) % 8)) == state && conf.get((r, (i + 2) % 8)) == state)
+            || (conf.get((r, (i + 8 - 2) % 8)) == state && conf.get((r, (i + 8 - 1) % 8)) == state)
     } else {
-        (conf.get_field_state(FieldPos{ring_index, index: (i + 1) % 8}) == state
-            && conf.get_field_state(FieldPos{ring_index, index: (i + 8 - 1) % 8}) == state)
-        || (conf.get_field_state(FieldPos{ring_index: 0, index: i}) == state
-            && conf.get_field_state(FieldPos{ring_index: 1, index: i}) == state
-            && conf.get_field_state(FieldPos{ ring_index: 2, index: i}) == state)
+        (conf.get((r, (i + 1) % 8)) == state && conf.get((r, (i + 8 - 1) % 8)) == state)
+            || (conf.get((0, i)) == state && conf.get((1, i)) == state && conf.get((2, i)) == state)
     }
-    /*
-    let FieldPos{ring_index: _r, index: i} = p;
-    let state = conf.get_field_state(p);
-
-    let rep_indices_to_rotate = (((p.index - (p.index % 2) + 7) % 8) * 2) as u32;
-    // Field state triple containing field_index:
-    let state_triple = conf.state[p.ring_index].rotate_right(rep_indices_to_rotate) & 0b0000_0000_0011_1111u16;
-
-    /* 010101 | 101010 */
-    if state_triple == 21u16 || state_triple == 42u16 {
-        return true;
-    }
-
-    // If index is located in an edge, two triples must be checked for mill occurrence
-    return if is_corner(p) {
-        let state_triple = conf.state[p.ring_index].rotate_right(p.index * 2) & 0b0000_0000_0011_1111u16;
-        (state == FieldState::White && state_triple == 21u16) || (state == FieldState::Black && state_triple == 42u16)
-    } else {
-        conf.get_field_state(FieldPos{ring_index: 0, index: i}) == conf.get_field_state(FieldPos{ring_index: 1, index: i})
-        && conf.get_field_state(FieldPos{ ring_index: 1, index: i}) == conf.get_field_state(FieldPos{ ring_index: 2, index: i})
-        && conf.get_field_state(FieldPos{ ring_index: 0, index: i}) == state
-    }
-     */
 }
 
-fn has_pieces_not_in_muhle(conf: &EfficientPlayField, color: FieldState) -> bool {
-    POSITIONS.iter().copied().filter(|&p| conf.get_field_state(p) == color).any(|p| !is_muhle(&conf, p))
+fn has_pieces_not_in_muhle(conf: &Configuration, color: State) -> bool {
+    POSITIONS.iter().copied().filter(|&p| conf.get(p) == color).any(|p| !is_muhle(&conf, p))
 }
 
-pub fn count_stones_of_color(conf: &EfficientPlayField, color: FieldState) -> usize {
-    POSITIONS.iter().copied().filter(|&p| conf.get_field_state(p) == color).count()
+pub fn count(conf: &Configuration, color: State) -> usize {
+    POSITIONS.iter().copied().filter(|&p| conf.get(p) == color).count()
 }
 
-fn for_each_neighbour<F>(p: FieldPos, mut func: F)
-    where F: FnMut(FieldPos) {
-    let FieldPos{ring_index: r, index: i} = p;
-    func(FieldPos{index: (i + 1) % 8, ..p});
-    func(FieldPos { index: (i + 8 - 1) % 8,..p});
+fn for_each_neighbour<F>(p: Pos, mut func: F)
+    where F: FnMut(Pos) {
+    let (r, i) = p;
+    func((r, (i + 1) % 8));
+    func((r, (i + 8 - 1) % 8));
     if !is_corner(p) {
         if r != 2 {
-            func(FieldPos{ring_index: r + 1, ..p});
+            func((r + 1, i));
         }
         if r != 0 {
-            func(FieldPos{ring_index: r - 1, ..p});
+            func((r - 1, i));
         }
     }
 }
 
-fn for_each_reachable_field<F>(conf: &EfficientPlayField, p: FieldPos, mut func: F)
-    where F: FnMut(FieldPos) {
-    let can_jump = count_stones_of_color(conf, conf.get_field_state(p)) == 3;
+fn for_each_reachable<F>(conf: &Configuration, p: Pos, mut func: F)
+    where F: FnMut(Pos) {
+    let can_jump = count(conf, conf.get(p)) == 3;
     if can_jump {
-        POSITIONS.iter().copied().filter(|&p| conf.get_field_state(p) == FieldState::Free).for_each(func);
+        POSITIONS.iter().copied().filter(|&p| conf.get(p) == State::Empty).for_each(func);
     } else {
         for_each_neighbour(p, |p| {
-            if conf.get_field_state(p) == FieldState::Free {
+            if conf.get(p) == State::Empty {
                 func(p);
             }
         });
     }
 }
 
-fn for_each_action<F>(conf: &EfficientPlayField, phase: Phase, color: FieldState, mut func: F)
+fn for_each_action<F>(conf: &Configuration, phase: Phase, color: State, mut func: F)
     where F: FnMut(Action) {
     match phase {
         Phase::Placement => {
-            POSITIONS.iter().copied().filter(|&p| conf.get_field_state(p) == FieldState::Free).for_each(|p| {
+            POSITIONS.iter().copied().filter(|&p| conf.get(p) == State::Empty).for_each(|p| {
                 func(Action::Place(p));
             });
         }
         Phase::Moving => {
-            POSITIONS.iter().copied().filter(|&p| conf.get_field_state(p) == color).for_each(|from| {
-                for_each_reachable_field(conf, from, |to| {
+            POSITIONS.iter().copied().filter(|&p| conf.get(p) == color).for_each(|from| {
+                for_each_reachable(conf, from, |to| {
                     func(Action::Move(from, to));
                 })
             });
@@ -109,36 +79,36 @@ fn for_each_action<F>(conf: &EfficientPlayField, phase: Phase, color: FieldState
     }
 }
 
-pub fn compute_actions(conf: &EfficientPlayField, phase: Phase, color: FieldState) -> Vec<Action> {
+pub fn compute_actions(conf: &Configuration, phase: Phase, color: State) -> Vec<Action> {
     let mut res = Vec::new();
     for_each_action(conf, phase, color, |a| res.push(a));
     res
 }
 
-fn for_each_can_take<F>(conf: &EfficientPlayField, color: FieldState, func: F)
-    where F: FnMut(FieldPos) {
-    let black_has_takable_pieces = has_pieces_not_in_muhle(conf, /* ! */color); // TODO
-    POSITIONS.iter().copied().filter(|&p| conf.get_field_state(p) == color)
-        .filter(|&p| !black_has_takable_pieces || !is_muhle(conf, p))
+fn for_each_can_take<F>(conf: &Configuration, color: State, func: F)
+    where F: FnMut(Pos) {
+    let black_has_pieces_not_in_muhle = has_pieces_not_in_muhle(conf, color);
+    POSITIONS.iter().copied().filter(|&p| conf.get(p) == color)
+        .filter(|&p| !black_has_pieces_not_in_muhle || !is_muhle(conf, p))
         .for_each(func);
 }
 
-pub fn compute_can_take(conf: &EfficientPlayField, color: FieldState) -> Vec<FieldPos> {
+pub fn compute_can_take(conf: &Configuration, color: State) -> Vec<Pos> {
     let mut res = Vec::new();
     for_each_can_take(conf, color, |p| res.push(p));
     res
 }
 
-fn for_each_move<F>(conf: &EfficientPlayField, phase: Phase, color: FieldState, mut func: F)
+fn for_each_move<F>(conf: &Configuration, phase: Phase, color: State, mut func: F)
     where F: FnMut(Move) {
     for_each_action(conf, phase, color, |a| {
-        let successor = apply_action(conf, a, !color);
+        let successor = apply_action(conf, a, color);
         let can_take = is_muhle(&successor, match a {
             Action::Place(p) => { p }
             Action::Move(_from, to) => { to }
         });
         if can_take {
-            for_each_can_take(conf, !color, |take| { // TODO not &successor?
+            for_each_can_take(conf, color.flip_color(), |take| {
                 func(Move {
                     action: a,
                     take: Some(take),
@@ -153,7 +123,7 @@ fn for_each_move<F>(conf: &EfficientPlayField, phase: Phase, color: FieldState, 
     });
 }
 
-pub fn compute_moves(conf: &EfficientPlayField, phase: Phase, color: FieldState) -> Vec<Move> {
+pub fn compute_moves(conf: &Configuration, phase: Phase, color: State) -> Vec<Move> {
     let mut res = Vec::new();
     for_each_move(conf, phase, color, |m| res.push(m));
     res
@@ -175,24 +145,18 @@ fn main() {
         };
 
         let color = match input.next().unwrap() {
-            "B" => FieldState::Black,
-            "W" => FieldState::White,
+            "B" => State::Black,
+            "W" => State::White,
             _ => panic!("Unknown color")
         };
 
-        let conf = EfficientPlayField::from_coded(input.next().unwrap());
+        let conf: Configuration = input.next().unwrap().parse().unwrap();
 
         let moves = compute_moves(&conf, phase, color);
         let i = unsafe {
            RNG.generate_range(0..moves.len())
         };
         let selected = moves[i];
-
-        eprintln!("{conf}\n---");
-        for m in moves {
-            eprintln!("{}", m.to_string())
-        }
-
         println!("{}", selected.to_string());
     }
 }
